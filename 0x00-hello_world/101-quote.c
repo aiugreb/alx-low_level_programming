efc3c583b91675e46c86a29a1a4cc91cf3233c15
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-05 16:25:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Print: the quote
+ * Return: Always 1 (Success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Kor par, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+
+}
