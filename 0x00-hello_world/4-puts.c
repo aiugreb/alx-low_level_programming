154554cace1e849b36a8e1446211b9e50924ecd6
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-05 14:34:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints with put function
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
