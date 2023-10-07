331233ea6d222c7c7cd20943f00c01145831c662
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-06 17:41:25 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print: numbers of base16 in lowercase
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
