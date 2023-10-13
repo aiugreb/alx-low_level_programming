ff17d1056e4d8fa83cbe34aff0616a4f8f708a3e
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-12 17:59:29 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers in 10 lines
+ * return: void
+*/
+
+void more_numbers(void);
+{
+	int i, j, k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			k = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar(k + 48);
+		}
+		_putchar('\n')
+	}
+}
