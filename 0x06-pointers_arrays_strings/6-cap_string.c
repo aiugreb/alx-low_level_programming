4c11fe66211f2bfcfe62fb313ee25196b6d8e65c
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-18 10:36:49 -0700

    debugging

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
- * Return: A pointer to the changed string.
- */
+ * Return: String with all words capitalized.
+*/
+
 char *cap_string(char *str)
 {
 	int i, j;
