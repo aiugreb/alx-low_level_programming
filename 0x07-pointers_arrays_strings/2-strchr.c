f0493375d0c5d0e52a5971d3db8e4b2007e59839
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-23 07:31:27 -0700

    debugtask2#

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - main function
  * @s: Function parameter
  * @c: Function parameter
- * Return: NULL
+ * Return: 0
 */
 
 char *_strchr(char *s, char c)
@@ -16,5 +16,5 @@ char *_strchr(char *s, char c)
 	if (s[i] == c)
 	return (s + i);
 	}
-	return (NULL);
+	return (0);
 }
