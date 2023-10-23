8b7df47b5493a389a727c0090c2126e02359f115
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-23 09:06:41 -0700

    debug task7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,13 +8,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-			_putchar(' ');
-		}
-		_putchar('\n');
+		for (j = 0; j < 8; j++)
+		_putchar(a[i][j]);
+		putchar('\n');
 	}
 }
