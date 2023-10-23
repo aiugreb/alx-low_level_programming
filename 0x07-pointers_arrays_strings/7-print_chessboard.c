2f0f1c1d8b5428f942baa90fb94b214bb51478db
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-10-23 09:01:03 -0700

    tsk6 debug

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,6 +5,7 @@
  * @a: function parameter declare
  * Return: Always 0.
 */
+
 void print_chessboard(char (*a)[8])
 {
 	for (int i = 0; i < 8; i++)
@@ -12,6 +13,7 @@ void print_chessboard(char (*a)[8])
 		for (int j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
