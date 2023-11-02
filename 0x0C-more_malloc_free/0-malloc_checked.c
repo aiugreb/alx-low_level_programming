8db779772f4513030216f2012ed87d2cf264a6c3
mohamedamineberguia <mohamedamineberguia@gmail.com>
2023-11-02 08:30:48 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - function that allocates memory
@@ -11,7 +12,9 @@ void *malloc_checked(unsigned int b)
 	int *x = malloc(b);
 
 	if (x == 0)
-		exit 98;
+	{
+		exit(98);
+	}
 
 	return (x);
 }
