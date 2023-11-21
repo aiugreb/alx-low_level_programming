864de8c677413205cb0ee82bb990369d65670325
Mohamed Amine BERGUIA <mohamedamineberguia@gmail.com>
2023-11-20 17:17:34 -0800

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,9 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *node = head;
+	listint_t *node;
 
-	for (node && i < index; node = node->next, i++)
+	for (node = head, node && i < index; node = node->next, i++)
 		;
 	return (node);
 }
