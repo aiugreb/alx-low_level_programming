9965217d85203060ed93143af1f0f84e99f7cd59
Mohamed Amine BERGUIA <mohamedamineberguia@gmail.com>
2023-11-21 05:20:53 -0800

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (i < index - 1)
 	{
-		if (!temp || !(node->next))
+		if (!node || !(node->next))
 			return (-1);
 		node = node->next;
 		i++;
