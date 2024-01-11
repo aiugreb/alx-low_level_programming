dfc45869586ecfe92b309c3b963d69d51aad4ee4
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-01-11 13:59:55 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,46 +3,45 @@
 /**
  * delete_dnodeint_at_index - deletes
  * deletes the node at index
- * @head: head of the list
- * @index: index of the new node 
+ * @head: head of double list
+ * @index: index where to insert
  * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *before, *del;
+	dlistint_t *nav, *before, *deleted;
 	unsigned int i = 0;
 
 	if (!head || !*head)
 		return (-1);
-	current = *head;
+	nav = *head;
 	if (index == 0)
 	{
-		*head = current->next;
-		(*head)->before = NULL;
-		free(current);
+		*head = nav->next;
+		(*head)->prev = NULL;
+		free(nav);
 		return (1);
 	}
-	while (current)
+	while (nav)
 	{
 		if (i == index)
-			del = current;
+			deleted = nav;
 		if (i == index - 1)
-			before = current;
+			before = nav;
 		if (i == index + 1)
 		{
-			before->next = current;
-			current->before = before;
-			free(del);
+			before->next = nav;
+			nav->prev = before;
+			free(deleted);
 			return (1);
 		}
-		if (current->next == NULL && i == index)
+		if (nav->next == NULL && i == index)
 		{
 			before->next = NULL;
-			free(current);
+			free(nav);
 			return (1);
 		}
-		current = current->next;
+		nav = nav->next;
 		i++;
 	}
 	return (-1);
