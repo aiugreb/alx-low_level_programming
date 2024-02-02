72574ac51f68ac970883f9fec705fa7594f02e7d
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-02-02 04:55:12 -0800

    debug task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,25 +6,22 @@
  * @size: size of the hash table
  * Return: the created hash table, or NULL if function fails
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_node_t **array;
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		array[i] = NULL;
-
-	table->array = array;
 	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
