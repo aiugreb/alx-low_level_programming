620f08f8fb7423409ed4b257c93c5a1889565ce9
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-05-08 18:45:23 -0700

    dsffe

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
 
 #endif
